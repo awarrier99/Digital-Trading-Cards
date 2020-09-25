94cc472bacffa756fb68c72ef663e93f3b264cdf
marianamatias <mariana@gatech.edu>
2020-09-24 20:06:36 -0400

    Issue #20 - created trading card widget on home page

--- src/ui/lib/screens/Home.dart
+++ src/ui/lib/screens/Home.dart
@@ -1,29 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:ui/components/Cards/TradingCard.dart';
+import 'package:ui/models/CardInfo.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 import '../palette.dart';
 import 'CreateCard1.dart';
 
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-class Home extends StatelessWidget {
+class _HomeState extends State<Home> {
+  int userID = 1;
+  Future<CardInfo> userCardInfo;
+
   Future createCard(context) async {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => CreateCard1()));
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => CreateCard1()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userCardInfo = fetchCardInfo(userID);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SizedBox(
-            child: RaisedButton(
-                child: Text('Create Card'),
-                textColor: Colors.white,
-                color: Palette.primaryGreen,
-                onPressed: () {
-                  createCard(context);
-                }
-            )
-        )
-      )
-    );
+        body: Container(
+            child: SizedBox(
+                child: Column(
+      children: [
+        RaisedButton(
+            child: Text('Create Card'),
+            textColor: Colors.white,
+            color: Palette.primaryGreen,
+            onPressed: () {
+              createCard(context);
+            }),
+        FutureBuilder<CardInfo>(
+          future: userCardInfo,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return TradingCard(snapshot.data);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ],
+    ))));
   }
 }
