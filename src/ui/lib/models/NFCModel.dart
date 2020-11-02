27d9bb719762fd857605c8fbe00757327509701a
Ashvin Warrier <warrierar@gmail.com>
2020-11-02 16:01:13 -0500

    Test nfc stream

--- src/ui/lib/models/NFCModel.dart
+++ src/ui/lib/models/NFCModel.dart
@@ -5,23 +5,33 @@ import 'package:nfc_in_flutter/nfc_in_flutter.dart';
 
 class NFCModel {
   void addUser(String username) async {
+    // final receivePort = ReceivePort();
+    // final map = {'port': receivePort.sendPort, 'username': username};
+    // final isolate = await Isolate.spawn(notifier, map);
+    // final sendPort = await receivePort.first;
+    // NDEFMessage message = await NFC.readNDEF(once: true).first;
+    // print(message.payload);
+    Stream<NDEFMessage> stream = NFC.readNDEF();
+    stream.listen((message) {
+      print(message.payload);
+      NDEFMessage newMessage = NDEFMessage.withRecords([
+        NDEFRecord.type('application/json', json.encode({'username': username}))
+      ]);
+      message.tag.write(newMessage);
+    });
+  }
+}
+
+Future<void> notifier(Map<String, dynamic> map) async {
+  try {
     final receivePort = ReceivePort();
-    final isolate = await Isolate.spawn(listener, receivePort.sendPort);
-    final sendPort = await receivePort.first;
+    final sendPort = map['port'];
+    final username = map['username'];
+
     final userMap = {'username': username};
     NDEFMessage message = NDEFMessage.withRecords(
         [NDEFRecord.type('application/json', json.encode(userMap))]);
     await NFC.writeNDEF(message, once: true).first;
-  }
-}
-
-Future<void> listener(SendPort sendPort) async {
-  try {
-    final receivePort = ReceivePort();
-    sendPort.send(receivePort.sendPort);
-
-    NDEFMessage message = await NFC.readNDEF(once: true).first;
-    print(message.payload);
   } catch (err, stacktrace) {
     print('An error occurred while trying to add a user with NFC');
     print(err);
