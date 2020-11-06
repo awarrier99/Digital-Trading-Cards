c9d4c597d4683dff6986270225bea9ca9603f239
Ashvin Warrier <warrierar@gmail.com>
2020-11-05 21:30:47 -0500

    Stop scan finally

--- src/ui/lib/models/BluetoothModel.dart
+++ src/ui/lib/models/BluetoothModel.dart
@@ -2,18 +2,21 @@ import 'package:flutter_blue/flutter_blue.dart';
 
 class BluetoothModel {
   void addUser(String username) async {
+    final flutterBlue = FlutterBlue.instance;
     try {
-      final flutterBlue = FlutterBlue.instance;
       flutterBlue.startScan();
       flutterBlue.scanResults.listen((results) {
         for (final r in results) {
           print(r.device.name);
+          print(r.device.type);
         }
       });
     } catch (err, stacktrace) {
       print('An error occurred while trying to add a user by Bluetooth:');
       print(err);
       print(stacktrace);
+    } finally {
+      flutterBlue.stopScan();
     }
   }
 }
